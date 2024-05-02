ef299831ca8afa32ec99a1e178fd29ff110551d8
KashigariGayathri <167190977+KashigariGayathri@users.noreply.github.com>
2024-05-02 13:40:51 +0530

    Add files via upload

---
+++ StaticVariableEx2.java
@@ -0,0 +1,42 @@
+package variables;
+class SubjectMarks {
+	// static variable 
+	public static int ScienceMarks;
+	public static int MathsMarks;
+	public static int GKMarks;
+	
+	}
+public class StaticVariableEx2 {
+
+	public static void main(String[] args) {
+		// Student 1
+				
+		SubjectMarks.ScienceMarks = 60; 
+		SubjectMarks.MathsMarks = 80; 
+		SubjectMarks.GKMarks = 70;
+				// Student 2
+		SubjectMarks.ScienceMarks = 65; 
+		SubjectMarks.MathsMarks = 85; 
+		SubjectMarks.GKMarks = 75;
+
+				// displaying marks for Student 1
+				System.out.println("Marks for first student:"); 
+				System.out.println(SubjectMarks.ScienceMarks); 
+				System.out.println(SubjectMarks.MathsMarks); 
+				System.out.println(SubjectMarks.GKMarks);
+				// displaying marks for Student 2
+				System.out.println("Marks for second object:"); 
+				System.out.println(SubjectMarks.ScienceMarks); 
+				System.out.println(SubjectMarks.MathsMarks); 
+				System.out.println(SubjectMarks.GKMarks);
+				/*this code doesn't work because  it does not create separate instances 
+				 * of the SubjectMarks class for each student. Instead, it uses static variables 
+				 * to store the marks, which means that when you assign new values to the 
+				 * variables, they get overwritten for all students. As a result, the marks
+				 *  displayed for the second student will be the same as those for the first
+				 *   student.To correct this error, you should create separate instances
+				 *    of the SubjectMarks class for each */
+
+	}
+
+}
